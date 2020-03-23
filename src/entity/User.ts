d2d9825f4ef24adcf7bb614abf8fce95540cf9bd
Jamie <jamiebshaw1095@gmail.com>
2020-03-23 11:32:35 +0000

    initial commit

---
+++ src/entity/User.ts
@@ -0,0 +1,22 @@
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
+
+@Entity()
+export class User extends BaseEntity {
+	@PrimaryGeneratedColumn()
+	id: number;
+
+	@Column()
+	firstName: string;
+
+	@Column()
+	lastName: string;
+
+	@Column()
+	username: string;
+
+	@Column("text", { unique: true })
+	email: string;
+
+	@Column()
+	password: string;
+}
